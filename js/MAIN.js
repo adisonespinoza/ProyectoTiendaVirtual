3368cdb40730a300ac60b00e6f51b74bdd3bf0de
adisonespinoza <adisonespinoza95@hotmail.com>
2020-11-02 22:17:53 -0300

    se trabajo en el diseño del resumen de precios, el video y la ubicacion geografica

--- js/MAIN.js
+++ js/MAIN.js
@@ -2,81 +2,156 @@
     "use strict";
 
     document.addEventListener('DOMContentLoaded', function() {
+        //Mapa Personalizado
+        var map = L.map('mapa').setView([-34.603338, -58.410637], 14);
 
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
 
-        //Calcular montos de camisas
+        L.marker([-34.603338, -58.410637]).addTo(map)
+            .bindPopup('Mapa Personalizado por Irene & Adison.<br> Abasto Shopping CABA')
+            .openPopup();
+        //Evento del video
+        const video = document.querySelector('.video')
+        video.addEventListener('ended', function() {
+            Swal.fire({
+                title: 'Desea ver el video de nuevo?',
+                text: "Por favor seleccione!",
+                icon: 'success',
+                showCancelButton: true,
 
-        var cantidad1 = document.getElementById('cantidad1');
-        var cantidad2 = document.getElementById('cantidad2');
-        var cantidad3 = document.getElementById('cantidad3');
-        var resumen = document.getElementById('resumen');
-        var suma = document.getElementById('suma-total');
-        var calcular = document.getElementById('calcular');
-        var total = document.getElementById('pagar');
-
-
-        total.addEventListener('click', function() {
-                if (total.value === ' ') {
-                    $alert('debes tomar producto')
-                }
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
             })
-            //if (document.getElementById('calcular')) {
+            var result = result;
+            if (result) {
+                video.play();
+            }
+        })
+        video.addEventListener('mouseover', function() {
+            video.play();
+        })
+    });
+    //Calcular montos de camisas
+
+    var cantidad1 = document.getElementById('cantidad1');
+    var cantidad2 = document.getElementById('cantidad2');
+    var cantidad3 = document.getElementById('cantidad3');
+    var cantidad4 = document.getElementById('cantidad4');
+    var cantidad5 = document.getElementById('cantidad5');
+    var cantidad6 = document.getElementById('cantidad6');
+    var cantidad7 = document.getElementById('cantidad7');
+    var cantidad8 = document.getElementById('cantidad8');
+    var cantidad9 = document.getElementById('cantidad9');
+    var cantidad10 = document.getElementById('cantidad10');
+    var cantidad11 = document.getElementById('cantidad11');
+    var cantidad12 = document.getElementById('cantidad12');
+    var cantidad13 = document.getElementById('cantidad13');
+    var cantidad14 = document.getElementById('cantidad14');
+    var resumen = document.getElementById('lista_productos');
+    var suma = document.getElementById('suma-total');
+    var calcular = document.getElementById('calcular');
+    var total = document.getElementById('pagar');
+
+    total.addEventListener('click', function() {
+        Swal.fire({
+            type: 'error',
+            title: 'Oops... Ha ocurrido un error',
+            text: 'Para poder realizar el calculo, deber seleccionar almenos un producto    !',
+
+        })
+
         calcular.addEventListener('click', calcularMontos);
 
-        // }
+        function calcularMontos(e) {
+            e.preventDefault();
 
-        function calcularMontos(event) {
-            event.preventDefault();
-            // console.log('has hecho clic');
-            if (calcular.value === '') {
-                alert('debes elegir un regalo');
+            if (calcular === '') {
+
                 calcular.focus();
             } else {
-                console.log(cantidad1.value);
-                console.log(cantidad2.value);
-                console.log(cantidad3.value);
-            }
-            /* Swal.fire({
-                 type: 'error',
-                 title: 'Oops... Ha ocurrido un error',
-                 text: 'Para poder realizar el calculo, deber seleccionar almenos un producto    !',
+                var c1 = cantidad1.value,
+                    c2 = cantidad2.value,
+                    c3 = cantidad3.value,
+                    c4 = cantidad4.value,
+                    c5 = cantidad5.value,
+                    c6 = cantidad6.value,
+                    c7 = cantidad7.value,
+                    c8 = cantidad8.value,
+                    c9 = cantidad9.value,
+                    c10 = cantidad10.value,
+                    c11 = cantidad11.value,
+                    c12 = cantidad12.value,
+                    c13 = cantidad13.value,
+                    c14 = cantidad14.value;
+                var totalPagar = (c1 * 25) + (c2 * 25) + (c3 * 25) + (c4 * 25) + (c5 * 25) + (c6 * 25) + (c7 * 25) + (c8 * 25) + (c9 * 25) + (c10 * 25) + (c11 * 25) + (c12 * 25) + (c13 * 25) + (c14 * 25);
 
-             })*/
+                var listadoProductos = [];
+                if (c1 >= 1) {
+                    listadoProductos.push(c1 + ' VueJs');
 
-
-
-            /* } else {
-                    Swal.fire({
-                        type: 'error',
-                        title: 'Oops... Ha ocurrido un error',
-                        text: 'Para poder realizar el calculo, deber seleccionar almenos un producto    !',
-
-                    })
                 }
-            };
-            var totalPagar = (cantidad1 * 25) + (cantidad2 * 25) + (cantidad3 * 25);
+                if (c2 >= 1) {
+                    listadoProductos.push(c2 + ' AngularJs');
 
-            var listadoProductos = [];
+                }
+                if (c3 >= 1) {
+                    listadoProductos.push(c3 + ' React');
 
-            if (cantidad1 >= 1) {
-                listadoProductos.push(cantidad1);
+                }
+                if (c4 >= 1) {
+                    listadoProductos.push(c4 + ' Redux');
+
+                }
+                if (c5 >= 1) {
+                    listadoProductos.push(c5 + ' NodeJs');
+
+                }
+                if (c6 >= 1) {
+                    listadoProductos.push(c6 + ' Sass');
+
+                }
+                if (c7 >= 1) {
+                    listadoProductos.push(c7 + ' HTML5');
+
+                }
+                if (c8 >= 1) {
+                    listadoProductos.push(c8 + ' GitHub');
+
+                }
+                if (c9 >= 1) {
+                    listadoProductos.push(c9 + ' Bulma');
+
+                }
+                if (c10 >= 1) {
+                    listadoProductos.push(c10 + ' TypeScript');
+
+                }
+                if (c11 >= 1) {
+                    listadoProductos.push(c11 + ' Drupal');
+
+                }
+                if (c12 >= 1) {
+                    listadoProductos.push(c12 + ' JavaScript');
+
+                }
+                if (c13 >= 1) {
+                    listadoProductos.push(c13 + ' GraphQL');
+
+                }
+                if (c14 >= 1) {
+                    listadoProductos.push(c14 + ' WordPress');
+
+                }
+                resumen.style.display = "block";
+                resumen.innerHTML = '';
+                for (var i = 0; i < listadoProductos.length; i++) {
+                    resumen.innerHTML += listadoProductos[i] + '</br>';
+                }
+                suma.innerHTML = "$" + totalPagar.toFixed(2);
 
             }
-            if (cantidad2 >= 1) {
-                listadoProductos.push(cantidad2);
-
-            }
-            if (cantidad3 >= 1) {
-                listadoProductos.push(cantidad3);
-
-            }
-            resumen.innerHTML = "";
-            for (var i = 0; i < listadoProductos.length; i++) {
-                resumen.innerHTML += listadoProductos[i] + '</br>';
-            }
-            suma.innerHTML = "$" + totalPagar;*/
-
-
         };
-    })
+    });
 })();
